f2b0183cf30273cba26dcb2cedb724f3e56df779
Beverly Kololi <beverly.kololi@andela.com>
2020-04-21 13:13:42 +0300

    Initialize mapbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import useSWR from "swr"; // React hook to fetch the data
+import lookup from "country-code-lookup"; // npm module to get ISO Code for countries
+
+import "./App.scss";
+
+// Mapbox css - needed to make tooltips work later in this article
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYmV2a29sb2xpIiwiYSI6ImNrOTQyM216cjAxbHgzcWxnYjMyNGNrdXUifQ.ek8ioeDEErkXLCOZ7YC-Ww";
 
 function App() {
+  const mapboxElRef = useRef(null); // DOM element to render map
+
+  // Initialize our map
+  useEffect(() => {
+    // You can store the map instance with useRef too
+    const map = new mapboxgl.Map({
+      container: mapboxElRef.current,
+      style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
+      center: [16, 27], // initial geo location
+      zoom: 2 // initial zoom
+    });
+
+    // Add navigation controls to the top right of the canvas
+    map.addControl(new mapboxgl.NavigationControl());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mapContainer">
+        {/* Assigned Mapbox container */}
+        <div className="mapBox" ref={mapboxElRef} />
+      </div>
     </div>
   );
 }
